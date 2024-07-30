65e6bffe792c14723a8cf9a1663aa9efbefa72f8
shivamsingh1512 <bwubta21169@brainwareuniversity.ac.in>
2024-07-30 22:11:00 +0530

    added searching feature

--- src/components/body.js
+++ src/components/body.js
@@ -6,6 +6,11 @@ import Shimmer from "./Shimmer"
 const Body = () => {
 //local state variable- super powerful variable  yani filter wala auto ui change karne 
     const [listOfRestaurants , setListRestraunt] = useState([]);
+    const [ filterRestaurant , setFilterRestarunt ] = useState([]) ;
+
+    const [searchText , setsearchText] = useState("");
+
+
     useEffect(()=>{
        
         fetchData();
@@ -15,9 +20,10 @@ const Body = () => {
         const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.51800&lng=88.38320&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
 
         const json = await data.json();
-        console.log(json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants);
-        //optinal chaining
+        console.log(json);
+        //optinal chaining ?
         setListRestraunt(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+        setFilterRestarunt(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
     }; 
     
     return listOfRestaurants.length === 0 ? (
@@ -25,6 +31,21 @@ const Body = () => {
     ): (
         <div className="body">
         <div className="filter">
+            <div className="search">
+                <input type="text" className="search-box" placeholder="search" value={searchText} onChange={(e) => {
+                    setsearchText(e.target.value);
+                }}/>
+                <button onClick={ () => {
+                    console.log(searchText);
+                    const filterRestaurant = listOfRestaurants.filter(
+                        (res) => res?.info?.name.toLowerCase().includes(searchText.toLowerCase()) 
+                    );
+                    setListRestraunt (filterRestaurant);
+                }}
+                >
+                    search
+                </button>
+            </div>
             <button className="filter-btn" 
             onClick={() => {
                 // filter logic here of top rate restaurants
@@ -37,7 +58,7 @@ const Body = () => {
             </button>
         </div>
         <div className="res-container">
-            {listOfRestaurants.map((restaurant) => (
+            {filterRestaurant.map((restaurant) => (
                 <RestaurantCard key={restaurant.info.id} resData={restaurant} /> // looping the restaurantcard dynamic.... and key is every card have unique value.
             ))}
         </div>
